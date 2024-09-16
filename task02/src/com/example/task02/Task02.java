853077c0573e4fc2932c8ad1119280cb7e87581b
LilChooch <oleg.2018.85.ru@gmail.com>
2024-09-16 17:15:05 +0500

    Задание 02 Смолин Олег При-202

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -1,21 +1,58 @@
 package com.example.task02;
 
-public class Task02 {
 
-    public static String solution(String input) {
+import java.util.Scanner;
+public class Task02
+{
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+    public static String solution(String   input)
+    {
 
-        return "";
+        double x =  Double.valueOf(input);
+        String s = "";
+        if( x >= -128 && x <= 127 )
+        {
+            byte y = 0;
+            x = y;
+            s = "byte";
+        }
+        else if( x >= -32768 && x <= 32767)
+        {
+            short y = 0;
+            x = y;
+            s = "short";
+        }
+        else if( x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE)
+        {
+            int y = 0;
+            x = y;
+            s = "int";
+        }
+        else if( x >= Long.MIN_VALUE && x <= Long.MAX_VALUE)
+        {
+            long y = 0;
+            x = y;
+            s = "long";
+        }
+        else if( x >= Float.MIN_VALUE && x <= Float.MAX_VALUE)
+        {
+            float y = 0;
+            x = y;
+            s = "float";
+        }
+        else if( x >= Double.MIN_VALUE && x <= Double.MAX_VALUE)
+        {
+            s = "double";
+        }
+
+        return s;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         String result = solution("12345");
         System.out.println(result);
-         */
+
     }
 
 }
