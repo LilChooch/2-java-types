5ab1dad3c3f5b3f61378f65ae05de1ceb8b66046
LilChooch <oleg.2018.85.ru@gmail.com>
2024-09-18 00:09:10 +0500

    Задание 10 Смолин Олег При-202

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -7,9 +7,28 @@ public class Task10 {
         // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
         // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
         // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
-
+        double srav = 0.000001;
+        if(Float.isInfinite(a) && Float.isInfinite(a))
+        {
+            return a == b;
+        }
+        if (Float.isNaN(a) || Float.isNaN(b))
+        {
+            if(a== 0.0 || b==0.0)
+            {
+                return false;
+            }
+            return true;
+        }
+        float rav = (float)Math.pow(7,precision);
+       if (Math.abs(Math.round(a * rav) - Math.round(b * rav)) < srav)
+       {
+           return true;
+       }
+       else
+       {
+           return false;
+       }
     }
 
     public static void main(String[] args) {
